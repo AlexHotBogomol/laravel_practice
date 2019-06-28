5fbd14e6868a46637e47d2e32d040c947b305d9a
AlexHotBogomol <Zakablukov777@gmail.com>
2019-06-28 17:38:45 +0300

    book_catalog complited

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -6,10 +6,11 @@
         </div>
         <form>
             <div class="card-body">
-                <h5 class="card-title">Book title</h5>
-                <p class="card-text">Book author</p>
+                <h5 class="card-title">{{ $book->title }}</h5>
+                <p class="card-text">{{ $book->author }}</p>
                 <p class="card-text">Book content</p>
-                <button href="#" class="btn btn-primary">Delete</button>
+                <a href="{{ route('delete_book', ['id' => $book->id])}}" class="btn btn-primary">Delete</a>
+                <a href="{{ route('edit_book', ['id' => $book->id])}}" class="btn btn-dark">Edit</a>
             </div>
         </form>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'BookController@index')->name('home');
+Route::get('/book', 'BookController@create')->name('create');
+Route::post('/book', 'BookController@store')->name('create_book');
+Route::get('/books', 'BookController@index')->name('list_books');
+Route::get('/book/{id}', 'BookController@show')->name('show_book');
+Route::get('/books/{id}/delete', 'BookController@destroy')->name('delete_book');
+Route::get('/books/{id}/edit', 'BookController@edit')->name('edit_book');
+Route::post('/books/{id}/update', 'BookController@update')->name('update_book');
